dbc3267a1970148f7887e103283417fef2cb4228
richie48 <57833496+richie48@users.noreply.github.com>
2022-04-10 23:45:01 +0100

    grid object

---
+++ Grid.js
@@ -0,0 +1,27 @@
+const GRID_SIZE = 4;
+const CELL_SIZE = 20;
+const CELL_GAP = 2;
+
+export default class Grid {
+  constructor(gridElement) {
+    const GRID_SIZE = 4;
+    const CELL_SIZE = 20;
+    const CELL_GAP = 2;
+    gridElement.style.setProperty("--grid-size", GRID_SIZE);
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+
+    createCellElement(gridElement);
+  }
+}
+
+function createCellElement(gridElement) {
+  const cells = [];
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cells.push(cell);
+    gridElement.append(cell);
+  }
+  return cells;
+}
